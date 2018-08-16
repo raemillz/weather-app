622bb17dc7629ea7b4c7c2cce69eacbe1fe79df1
Rachel Miller <raeanne1991@gmail.com>
2018-08-16 12:44:53 -0400

    fixed current time

--- src/components/CurrentForcast.js
+++ src/components/CurrentForcast.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import moment from 'moment';
 
 const CurrentForcast = ({ forcast: { apparentTemperature, precipProbability, humidity, summary, temperature, time}}) =>
 <div>
   <h2>Current Forcast</h2>
   <div style={{ border: 'solid 1px black', padding: '16px', margin:'16px'}}>
     <h3>{summary}</h3>
-    <p>Current Time: {time}</p>
+    <p>{moment.unix(time).format('MMMM Do YYYY, h:mm:ss a')}</p>
     <p>Temperature: {Math.round(temperature)}*</p>
     <p>Feels Like: {Math.round(apparentTemperature)}*</p>
     <p>Chance of Precipitation: {precipProbability}</p>
