31851048f86f410345eb0c8676983d8884e92059
Rachel Miller <raeanne1991@gmail.com>
2018-08-16 12:52:22 -0400

    created nav buttons and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const Navbar = () => <div> Navbar is coming soon </div>
+const Navbar = () =>
+  <div>
+    <button className="nav-button">Current</button>
+    <button className="nav-button">Minutely</button>
+    <button className="nav-button">Hourly</button>
+    <button className="nav-button">Daily</button>
+  </div>
 
 export default Navbar;
