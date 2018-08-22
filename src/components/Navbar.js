ca8723727d7ae960a3288489eca668e78aa8811f
Rachel Miller <raeanne1991@gmail.com>
2018-08-22 17:49:31 -0400

    added semantic ui menu and centered it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,14 @@ import { Menu } from 'semantic-ui-react';
 
 const Navbar = ({ changeRoute }) =>
   <div>
-    <Menu>
+    <Menu className="compact">
       <Menu.Item onClick={() => changeRoute('currently')} name='Current'>
       </Menu.Item>
       <Menu.Item onClick={() => changeRoute('minutely')} name='Minutely'>
       </Menu.Item>
       <Menu.Item onClick={() => changeRoute('hourly')} name='Hourly'>
       </Menu.Item>
-      <Menu.Item onClick={() => changeRoute('daily')} name='Daily'>
+      <Menu.Item className='center aligned' onClick={() => changeRoute('daily')} name='Daily'>
       </Menu.Item>
       {/*<button className="nav-button" onClick={() => changeRoute('currently')}>Current</button>
       <button className="nav-button" onClick={() => changeRoute('minutely')}>Minutely</button>
