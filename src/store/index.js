f0305366d4a1e2917ccce75d5cff8c4c19d432dd
Rachel Miller <raeanne1991@gmail.com>
2018-08-22 14:10:50 -0400

    added combineReducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import fetchingData from '../reducers/fetchingDataReducer';
 import weatherData from '../reducers/weatherDataReducer';
 import route from '../reducers/routeReducer';
 
 const middlewares = [thunk];
+const reducers = combineReducers({
+  fetchingData,
+  weatherData,
+  route
+});
+
 export default createStore(
+  reducers,
   applyMiddleware(...middlewares)
 )
